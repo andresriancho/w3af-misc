b90dbff00111acc160a42fad11977f181258e3a9
Andres Riancho <andres.riancho@gmail.com>
2012-03-06 19:21:37 +0000

    Now, the real version.

--- misc/loose_encoding.py
+++ misc/loose_encoding.py
@@ -14,6 +14,9 @@ class LooseEncodingChecker(BaseChecker):
     msgs = {'E3731': ('Use of encoding/decoding WITHOUT specific character encoding.',
                       ('When using unicode() , "".encode() and "".decode() it is required'
                        ' to use a specific encoding instead of the default.')),
+            'W3732': ('Use of errors="ignore" in encode/decode functions',
+                      ('When using unicode(), "".encode() and "".decode() it is NOT recommended'
+                       ' to use errors="ignore". URL encode the character instead.') )
             }
     options = ()
     # this is important so that your checker is executed before others
@@ -25,13 +28,29 @@ class LooseEncodingChecker(BaseChecker):
 
         See compiler.ast documentation for a description of available nodes:
         http://www.python.org/doc/current/lib/module-compiler.ast.html
+
+        I want to catch calls like this:
+            unicode('a')
+            unicode('a', errors='ignore')
+            'a'.decode()
+            'a'.decode(errors='ignore')
+            'a'.encode()
+            'a'.encode(errors='ignore')
+
+        And I do NOT want to catch calls like this:
+            'a'.decode('utf8')
+            'a'.encode('utf8')
+            unicode('a', 'utf8')
         """
-        print dir(node)
-        print node.args
-        self.add_message('W9901', line=1)
+        if isinstance(node.func, astng.Name):
+            name = node.func.name
+            if name == 'unicode':    
+                if len(node.args) == 1:
+                    self.add_message('E3731', line=node.lineno)
+                if len(node.args) == 1:
+                    self.add_message('E3731', line=node.lineno)
 
 def register(linter):
     """required method to auto register this checker"""
     linter.register_checker(LooseEncodingChecker(linter))
-    print 'reg!'
         
